b1da8cf834eb31d4d78caca8d5e1dcb5e53f56a0
Babar <69425648+nobabar@users.noreply.github.com>
2021-05-09 18:54:41 +0200

    Update test_power.R

--- test_power.R
+++ test_power.R
@@ -1,16 +1,16 @@
 ### test de la puissance du test t de student ###
 
 
-n=c(2, 6, 8, 10, 15, 30) #tailles des échantillons
+n=c(2, 6, 8, 10, 15, 30) #tailles des Ã©chantillons
 m=5      #moyenne
 e=1      #ecart type
 d=2      #distance max de la moyenne
-ni=10000 #nombre d'itérations du test
+ni=10000 #nombre d'itÃ©rations du test
 
 set.seed(1)
 
 
-                                                      #Itérations du t.test 
+                                                      #ItÃ©rations du t.test 
 
 ttest_ratio = function(ne, distance, n_iteration){
   ratio = array(0, dim=c(1, 2))
@@ -58,25 +58,4 @@ print(table_ratio)
 
 #export data to excel
 
-write_xlsx(table_ratio,"D:\\Data\\Fac\\L3\\S6\\Info-Stat\\Stat\\Project - Welch\\PowerR.xlsx")
-
-
-#Représentation des résultats
-
-# windows()
-# par(mfrow=c(2, 3))
-# p = ggplot() +
-#       geom_point(data = table_ratio, aes(x = delta, y = ttest)) +
-#       geom_point(data = table_ratio, aes(x = delta, y = welch)) +
-#       geom_line(data = table_ratio, aes(x = delta, y = ttest), size = 0.8, colour = "darkred") +
-#       geom_line(data = table_ratio, aes(x = delta, y = welch), size = 0.8, colour = "steelblue") +
-#       theme_bw() +
-#       xlab('factors') +
-#       ylab('power')
-
-
-
-
-
-
-
+write_xlsx(table_ratio,"C:\\User\\Username\\...\\PowerR.xlsx")
